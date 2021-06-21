856bddaa2e4997aac3c89e01c7de6d6f0231e1f8
Christian <pintochristian2@gmail.com>
2021-06-20 21:30:11 -0400

    Added the engineering projects from college to the data

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,4 +1,4 @@
-const projects = [
+const webProjects = [
     {
         name: 'Piriji',
         builtWith: 'Ruby on Rails / Postgresql / React / CSS / AWS / React Native',
@@ -19,5 +19,38 @@ const projects = [
         link: 'https://chrispinto1.github.io/Team-Ramdomizer/',
         thirdPartyAPIs: 'LOVO Voice API',
         description: 'An app that puts teams into groups randomly with animations and voice overs. It will save the result to local storage so on the next visit it will display the saved groups for future references.'
+    },
+]
+
+const engineeringProjects = [
+    {
+        name: 'Robotic Hand',
+        builtWith: 'Assembly / 3D Printed Parts / Servo Motors / Cyclone IV - Altera DE2 Board',
+        description: 'A robotic hand that is controlled by servo motors which are then controlled by outputs from push buttons on the Altera DE2 Board'
+    },
+    {
+        name: 'Survive NYC',
+        builtWith: 'Java',
+        description: 'An android game app about killing zombies to survive till you make it to the end. Project includes our own design and involves collison detection for both the zoombies, player, and bulltets.'
+    },
+    {
+        name: 'Deciphering/Ciphering Messages',
+        builtWith: 'Matlab',
+        description: 'A program written to decipher messages used by certain encryption algorithms as well as encrypt messages by the users choice'
+    },
+    {
+        name: 'Mr. P',
+        builtWith: 'C / Arduino / Sensors / Servo Motors / 8-bit LCD Display',
+        description: 'A smart parking lot that will inform drivers of how many parking spots are available in the garage to help save time from looking.'
+    },
+    {
+        name: 'Smart Parking Garage',
+        builtWith: 'C / Arduino / Photoresistors / Servo Motors / DC Motor / 8-Bit LCD Display',
+        description: 'A smart parking garage that will help you locate the closest parking spot available. If the first floor is full, it will open the elevator in which your car will go in and take you to the next available floor with parking spaces.'
     }
 ]
+
+export default {
+    webProjects,
+    engineeringProjects
+}
