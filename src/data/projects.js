665cd391fbacdaf92cafbb4c9eadbc535cce2ca8
Christian <pintochristian2@gmail.com>
2021-06-20 20:49:01 -0400

    added a few projects to the projects data list

---
+++ src/data/projects.js
@@ -0,0 +1,23 @@
+const projects = [
+    {
+        name: 'Piriji',
+        builtWith: 'Ruby on Rails / Postgresql / React / CSS / AWS / React Native',
+        link: 'https://www.piriji.com',
+        thirdPartyAPIs: 'Mapbox / Paypal / Google Reverse Geocoding (Mobile App)',
+        description: 'Pirji is a web and mobile app that allows you to find bars that have happy hour specials. It is currently specific to NYC only aat the moment with the outlook of expanding beyond NYC'
+    },
+    {
+        name: 'Popshop',
+        builtWith: 'Django / Python / Javascript / HTML / CSS / JQuery / Postgresql',
+        link: 'https://popshop.com',
+        thirdPartyAPIs: 'Google Maps / Stripe / Plaid / Owl Carousel',
+        description: 'Popshop is an app that allows you to book retail spaces to have your own brick and motar store. We utilize google maps to display and recommend spaces to business owners who may want to take their virutal buisness to a physical space.'
+    },
+    {
+        name: 'Group randomizer',
+        builtWith: 'Javascript / Jquery / HTML / CSS',
+        link: 'https://chrispinto1.github.io/Team-Ramdomizer/',
+        thirdPartyAPIs: 'LOVO Voice API',
+        description: 'An app that puts teams into groups randomly with animations and voice overs. It will save the result to local storage so on the next visit it will display the saved groups for future references.'
+    }
+]
